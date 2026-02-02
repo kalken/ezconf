c0c73896285c724b0c65cc5714a6cc96f70bc4d5
kalken <linux4dummies@gmail.com>
2026-02-02 22:11:26 +0100

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
-  description = "Nix development environment with Neovim, Alejandra, and nixd";
-
+  description = "ezconf - Neovim configuration package";
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
+  
   outputs = {
     self,
     nixpkgs,
@@ -15,20 +15,55 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      
+      ezconf = pkgs.stdenv.mkDerivation {
+        pname = "ezconf";
+        version = "1.0.0";
+        
+        src = ./.;
+        
+        nativeBuildInputs = with pkgs; [ makeWrapper ];
+        buildInputs = with pkgs; [ nixd alejandra ];
+        
+        installPhase = ''
+          mkdir -p $out/share/nvim
+          cp -r * $out/share/nvim/
+          
+          mkdir -p $out/bin
+          makeWrapper ${pkgs.neovim}/bin/nvim $out/bin/ezconf \
+            --add-flags "-u $out/share/nvim/init.lua" \
+            --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.nixd pkgs.alejandra ]} \
+            --add-flags "--cmd 'set runtimepath^=$out/share/nvim'"
+        '';
+        
+        meta = with pkgs.lib; {
+          description = "My Neovim configuration";
+          license = licenses.mit;
+          maintainers = [ ];
+        };
+      };
+      
     in {
+      packages.default = ezconf;
+      
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           neovim
           alejandra
           nixd
+          ezconf
         ];
-
+        
         shellHook = ''
           echo "Nix development environment loaded"
           echo "Available tools:"
           echo "  - nvim (Neovim)"
           echo "  - alejandra (Nix formatter)"
           echo "  - nixd (Nix language server)"
+          echo "  - ezconf (your configured Neovim)"
+          echo ""
+          echo "Build ezconf with: nix build"
+          echo "Run ezconf with: nix run (or just 'ezconf' in this shell)"
         '';
       };
     });
